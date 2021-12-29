156f3f6d55ad2f7a425f69de09ce88e37a46fc8e
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2021-12-29 20:59:14 +0100

    confirm email update

--- ClientApp/src/app/components/auth/confirm-email-components/confirm-email-failure/confirm-email-failure.component.ts
+++ ClientApp/src/app/components/auth/confirm-email-components/confirm-email-failure/confirm-email-failure.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-email-failure',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmEmailFailureComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigateTo(url: string): void {
+    this.router.navigate([url], { queryParams: { returnUrl: "/home" } });
+  }
 }
--- ClientApp/src/app/components/auth/confirm-email-components/confirm-email-success/confirm-email-success.component.ts
+++ ClientApp/src/app/components/auth/confirm-email-components/confirm-email-success/confirm-email-success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-email-success',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmEmailSuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigateTo(url: string): void {
+    this.router.navigate([url], { queryParams: { returnUrl: "/home" } });
+  }
 }
