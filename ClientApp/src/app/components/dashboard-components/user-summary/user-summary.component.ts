6a9b22c4bad36079e7643e92568537e3b27a7083
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-03 16:42:39 +0100

    add summary

--- ClientApp/src/app/components/dashboard-components/user-summary/user-summary.component.ts
+++ ClientApp/src/app/components/dashboard-components/user-summary/user-summary.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@app/models';
+import { AuthService, DataService } from '@app/services';
 
 @Component({
   selector: 'app-user-summary',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-summary.component.scss']
 })
 export class UserSummaryComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.userInfo().subscribe((data: any) => {
+      console.log("UserData: ");
+      console.log(JSON.stringify(data.body));
+      this.user = new User(data.body);
+      console.log(this.user);
+    });
   }
 
 }
