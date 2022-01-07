1adaeaf31335fcd358ac18475ec01087414820a0
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-07 20:25:28 +0100

    update wiki form layout

--- ClientApp/src/app/components/dashboard-components/wiki-form/wiki-form.component.ts
+++ ClientApp/src/app/components/dashboard-components/wiki-form/wiki-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Wiki } from '@app/models';
 import { AuthService, DataService } from '@app/services';
+import { faLeaf } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-wiki-form',
@@ -9,21 +11,54 @@ import { AuthService, DataService } from '@app/services';
   styleUrls: ['./wiki-form.component.scss']
 })
 export class WikiFormComponent implements OnInit {
-  form: any = {
-    name: null,
-    url: null,
-    description: null
-  };
-  isSuccessful = false;
-  isWikiCreateFailed = false;
+  form: FormGroup;
+
+  wiki: Wiki;
+  wikiUpdated: Wiki;
+
+  submitted:boolean = false;
+
+  isSuccessful:boolean = false;
+  isFailed:boolean = false;
+
   errorMessage = '';
+  isResetDone:boolean = false;
+
+
   returnUrl = '/';
 
+  isMainPartCollapsed:boolean = false;
 
-  constructor(private authService: AuthService, private dataService: DataService, private router: Router, private route: ActivatedRoute) { }
+  isWikiDataUpdated: boolean = true;
+
+  constructor(private authService: AuthService, private dataService: DataService, private router: Router, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';//FIXME:
+    this.resetData();
+    // this.wiki = new Wiki({});
+    // this.wikiUpdated = new Wiki({});
+    // this.form = this.formBuilder.group({
+    //   name: ['', Validators.required],
+    //   url: ['', Validators.required],
+    //   description: ['', Validators.required]
+    // });
+    // this.onChanges();
+  }
+
+  onChanges(): void {
+    this.form.valueChanges.subscribe(val => {
+      this.wikiUpdated.name = val.name;
+      this.wikiUpdated.url = val.url;
+      this.wikiUpdated.description = val.description;
+      if(!(JSON.stringify(this.wiki) === JSON.stringify(this.wikiUpdated))){
+        this.isWikiDataUpdated = true;
+      }
+      else{
+        this.isWikiDataUpdated = false;
+      }
+      console.log("lolx"+this.isWikiDataUpdated);
+    });
   }
 
 
@@ -31,23 +66,66 @@ export class WikiFormComponent implements OnInit {
     this.router.navigate([url], { queryParams: { returnUrl: this.returnUrl } });
   }
 
-  onSubmit(): void {
-    var wiki  = new Wiki(this.form);
-    this.authService.user.subscribe(x=> wiki.owner = x!)
+  onSubmit(){
+    this.submitted = true;
+    if(!this.form.valid) {
+      // alert('Please fill all the required fields to create an article!')
+      return false;
+    } else {
+      var wiki  = this.wikiUpdated;
+      console.log(this.wiki);
+      this.dataService.createWiki(wiki).subscribe(
+        data => {
+          // console.log(data);
 
+          this.isSuccessful = true;
+          this.isFailed = false;
+          this.resetAfterTimeout();
+          // this.router.navigate(['../']);
+        },
+        err => {
+          // console.log(err);
+          this.errorMessage = err.error;
+          this.isFailed = true;
+          this.resetAfterTimeout(false);
+        }
+      );
+      return true;
+    }
+  }
 
-    this.dataService.createWiki(wiki).subscribe(
-      data => {
-        // console.log(data);
-        this.isSuccessful = true;
-        this.isWikiCreateFailed = false;
-        this.router.navigate(['/home']);
-      },
-      err => {
-        // console.log(err);
-        this.errorMessage = err.error;
-        this.isWikiCreateFailed = true;
+  resetAfterTimeout(wipeData:boolean=true){
+    setTimeout( ()=>{
+      if(this.isResetDone){
+        this.isResetDone = false;
       }
-    );
+      else{
+        if(wipeData){this.resetData();}
+        this.submitted = false;
+        this.isWikiDataUpdated= false;
+        this.isSuccessful = false;
+        this.isFailed = false;
+      }
+    }, 5000)
+  }
+
+  resetForm(wipeData:boolean=true){
+    if(wipeData){this.resetData();}
+    this.submitted = false;
+    this.isWikiDataUpdated= false;
+    this.isSuccessful = false;
+    this.isFailed = false;
+    this.isResetDone = true;
+  }
+
+  resetData(){
+    this.wiki= new Wiki({});
+    this.wikiUpdated = new Wiki({});
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      url: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+    this.onChanges();
   }
 }
